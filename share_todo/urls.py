e3cd67c23894332d4ffb1fdc4ce751d947389331
Zhang Yuang <1328410180@qq.com>
2019-02-25 18:52:36 +0800

    Update: UI

--- share_todo/urls.py
+++ share_todo/urls.py
@@ -15,10 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('favicon.ico', RedirectView.as_view(url=r'static/favicon.ico')),
     path('', include('todo.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
